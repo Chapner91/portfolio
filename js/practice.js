2cb0a5ac922dd7aca3d38c1e4250bea80ce94cd7
Chapner91 <Chapner91@gmail.com>
2015-02-02 15:33:37 -0500

    played in js and css

--- js/practice.js
+++ js/practice.js
@@ -52,9 +52,67 @@ while (counter) {
 
 
 //for loop practice
-
+/*
 for(var counter = 10;counter;counter = counter - 1) {
 	console.log(counter)
 }
+*/
+
+//arrays
+/*console.log(friends);
+console.log(friends.length);
+
+var friendNumber = friends.length
+
+for (var i=0; i<friendNumber; i+=1) {
+	console.log(friends[i] + " is my friend");
+};
+*/
+
+
+
+//object practice
+/*var me = {
+	first_name: "Jason",
+	last_name: "Hapner",
+	age: 23
+}
+
+console.log(me)
+*/
+
+
+
+//function practice
+/*
+
+var sayHello = function () {
+	console.log("Hello!");
+	x+=1
+}
+
+var debug = function (message) {
+	console.log("Debug "+message);
+}
+var x = 0	
+sayHello();
+debug("my name is jason");
+sayHello();
+debug(x)
+sayHello()
+debug("this program tracks the number of times you say hello")
+debug("it has said hello: " + x + " times")
+*/
+
+var evenOdd = function (num, first, last) {
+	if (num%2 === 0) {
+		console.log(first + " " + last + " " + num + " is even")
+	}
+	else {
+		console.log(first + " " + last + " " + num + " is odd")
+	}
+}
+
+evenOdd(7, "Jason", "Hapner")
 
 console.log("End Script");
