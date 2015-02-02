6279ec388b2b8d20994dffa0331d7b3fca2500e7
Chapner91 <Chapner91@gmail.com>
2015-02-02 13:00:25 -0500

    js practice and navigation
    Practicing different js commands and updating the css on the navigation
    bar

---
+++ js/practice.js
@@ -0,0 +1,59 @@
+console.log("Begin Script");
+
+
+// this section establishes the users first name as the variable firstName
+
+/*
+var firstName = prompt("what is your name?");
+
+alert("Hello " + firstName);
+alert(firstName + " is a great programmer!");
+*/
+
+
+
+
+
+
+// if then statement practice
+
+/*
+console.log("Before");
+
+var age = prompt("how old are you?")
+
+if (age >= 18) {
+	console.log("You are old enough to access this website");
+}
+
+else if (age < 18) {
+	console.log("you are not old enough to access this website");
+}
+
+console.log("After");
+*/
+
+
+
+
+//while loop practice
+/*
+var counter = 10;
+
+while (counter) {
+	console.log("counter is active");
+	counter = counter - 1;
+}
+*/
+
+
+
+
+
+//for loop practice
+
+for(var counter = 10;counter;counter = counter - 1) {
+	console.log(counter)
+}
+
+console.log("End Script");
